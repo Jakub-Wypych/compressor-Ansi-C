c64f017aaf03f2b192a2d6af3d464da5662b475c
Jakub Wypych <01178898@pw.edu.pl>
2023-03-18 16:00:36 +0100

    preparing for compress.c #3 fixed the missing repeating probability in dictionary

--- src/compressor_tools.c
+++ src/compressor_tools.c
@@ -35,6 +35,29 @@ void read_dictionary(dictionary_t dictionary) {
         fprintf(stderr,"\n");
 }
 
+void add_repeating_prob(dictionary_t dictionary) {
+	int repeats = -1;
+	dictionary_t tmp = dictionary, pointer;
+	while(tmp->next != NULL) {
+		if(tmp->next->probability == tmp->probability) {
+			if(repeats == 5) {
+				pointer->probability = 15;
+				repeats = -1;
+			}
+			if(repeats == -1)
+				pointer = tmp->next;
+			repeats++;
+		} else {
+			if(repeats != -1)
+				pointer->probability = 10 + repeats;
+			repeats = -1;
+		}
+		tmp = tmp->next;
+	}
+	if(repeats != -1)
+		pointer->probability = 10 + repeats;
+}
+
 /* Creates a struct dicionary_t fom heap_t */
 dictionary_t make_dictionary(heap_t list) {
         dictionary_t head = malloc(sizeof(*head)); /* first node is temporary */
@@ -43,7 +66,7 @@ dictionary_t make_dictionary(heap_t list) {
         do {
                 new_node = malloc(sizeof(*new_node));
                 new_node->symbol = tmp->symbol;
-                new_node->probability = tmp->amount;
+		new_node->probability = tmp->amount;
                 /* attach new node and move to it */
                 pos->next = new_node;
                 pos = pos->next;
@@ -52,5 +75,6 @@ dictionary_t make_dictionary(heap_t list) {
         pos = head;
         head = head->next;
         free(pos);
+	add_repeating_prob(head);
         return head;
 }
